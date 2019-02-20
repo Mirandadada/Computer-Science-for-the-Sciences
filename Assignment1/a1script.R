ef8391171efef1d52a5528482db43a95e0ca9a19
Mirandadada <47798248+Mirandadada@users.noreply.github.com>
2019-02-20 04:11:30 -0500

    Add files via upload

--- Assignment1/a1script.R
+++ Assignment1/a1script.R
@@ -2,5 +2,8 @@ source("a1funs.R")
 
 
 data <- as.matrix(read.table("http://www.cs.utoronto.ca/~radford/csc120/a1data.txt"))
-set.seed(28)
+set.seed(1)
 find_pairings(data,tries=10,plot=TRUE) 
+set.seed(37)
+find_pairings(data,tries=10,plot=TRUE) 
+
